48620bfce8194398bdf819a3fbe9436b7028360b
valek <coronadev.king@gmail.com>
2020-05-31 15:43:12 +0300

    Adding border and layout to Radio Buttons panel

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -2,8 +2,19 @@ import javax.swing.*;
 
 public class MyPanel extends JPanel {
 
-    public MyPanel() {
+    private ButtonGroup buttonGroup = new ButtonGroup();
+    private JRadioButton radioButton;
 
+    public MyPanel(String title, String[] option) {
 
+        setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),title));
+        setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+
+        for (int i = 0; i < option.length; i++) {
+            radioButton = new JRadioButton(option[i]);
+            add(radioButton);
+            buttonGroup.add(radioButton);
+            radioButton.setSelected(i==0);
+        }
     }
 }
